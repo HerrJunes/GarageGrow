252252c89f0a57b03938b1e12ea9af0fd19d72c4
Junes <jonas.wuergler@hotmail.com>
2024-06-15 21:05:54 +0200

    asf

--- scenes/main/Time.gd
+++ scenes/main/Time.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var HOLD = false
+var timeMultiplier = 10
 
 var day = 1
 var timestamp = 180
@@ -27,11 +28,12 @@ func _physics_process(delta):
 	if HOLD:
 		return
 	
-	timestamp += 1*delta
+	timestamp += (1*delta) * timeMultiplier
 	get_node(SUN).rotation.z = timestamp*delta
 	#get_node(SUN).rotate_z(0.1)
 	
 	if timestamp >= 360:
 		timestamp = 0
+		day += 1
 	
 	showTime()
